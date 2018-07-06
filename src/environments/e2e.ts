c7d37ad1c3c984f9130d804b74139c52e6344033
r3trosteve <stevebschofield@gmail.comD>
2018-07-06 14:11:51 +0100

    revert port

--- src/environments/e2e.ts
+++ src/environments/e2e.ts
@@ -1,7 +1,7 @@
 import development from './dev';
 import { EnvironmentSchema } from './schema';
 
-export const port = process.env.PORT || 8013;
+export const port = 8013;
 export const host = `http://localhost:${port}`;
 
 /**
